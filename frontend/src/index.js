61cc78fbb4cf36668e55cd1e7be004543fadb667
raihanguntur <raihanguntur50@gmail.com>
2025-12-30 18:32:22 +0700

    fix logo, add silent console for production

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './index.css';
+import './utils/silentConsole';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
